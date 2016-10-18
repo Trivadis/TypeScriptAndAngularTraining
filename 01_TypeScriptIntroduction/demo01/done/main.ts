386da700f41f01f115ac6827ed5ccc3392ecc365
Thomas Claudius Huber <Thomas Claudius Huber>
2016-10-18 20:18:37 +0200

    Added demo01 for TypeScript

---
+++ 01_TypeScriptIntroduction/demo01/done/main.ts
@@ -0,0 +1,26 @@
+interface IFriend{
+   firstname:string;
+   lastname:string;
+}
+
+class Friend implements IFriend
+{
+    firstname:string;
+    lastname:string;
+    GetFullName(friend:IFriend):string
+    {
+        return friend.firstname + ' ' + friend.lastname;
+    }
+}
+
+function sortFriendsByName(friends:IFriend[])
+{
+    var result = friends.slice(0);
+    result.sort((x,y)=>
+    {
+        return x.firstname.localeCompare(y.firstname);
+    });
+    return result;
+}
+
+sortFriendsByName([{firstname:"Thomas",lastname:"Huber" }]);
---
+++ 01_TypeScriptIntroduction/demo01/starter/main.js
@@ -0,0 +1,9 @@
+function sortFriendsByFirstName(friends)
+{
+    var result = friends.slice(0);
+    result.sort(function(x,y)
+    {
+        return x.firstname.localCompare(y.firstname);
+    });
+    return result;
+}
