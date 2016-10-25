058b5202cff163c1b0a9aa9ee158aae21fc96897
Thomas Claudius Huber <Thomas Claudius Huber>
2016-10-25 22:46:27 +0200

    added decorators

---
+++ 09_TypeScript_Decorators/demo01_ClassDecorator/application.ts
@@ -0,0 +1,11 @@
+import {EditorData} from './editordata-decorator';
+
+@EditorData({
+    firstname:"Thomas",
+    lastname:"Huber"
+})
+export class Application
+{
+
+}
+
---
+++ 09_TypeScript_Decorators/demo01_ClassDecorator/editordata-decorator.ts
@@ -0,0 +1,16 @@
+import './node_modules/reflect-metadata/Reflect';
+
+export const EditorDataMetadataKey = "EditorDataKey";
+ 
+export function EditorData(data:EditorDataItem)
+{
+    return function TheReal(target:Function)
+    {
+        Reflect.defineMetadata(EditorDataMetadataKey,data,target);
+    }
+}
+
+export interface EditorDataItem{
+    firstname:string;
+    lastname:string;
+}
