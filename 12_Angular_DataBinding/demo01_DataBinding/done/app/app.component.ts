09269b84e4b4c32ff97cd8b6a2c9b1f95b31c6e5
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-10-27 15:05:05 +0200

    Added BindingCssClassesDemo

---
+++ 12_Angular_DataBinding/demo01_DataBinding/done/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Details of {{person.fullname | uppercase}}</h1>
+            <input type="text"
+           [(ngModel)]="person.fullname">`
+})
+export class AppComponent {
+  person: Person = { fullname: "Lara Croft" };
+}
+interface Person {
+  fullname: string
+}
