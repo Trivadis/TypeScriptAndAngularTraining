667ad99c7856abf9bd352e10cfe53db40477629b
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-11-01 10:58:03 +0100

    added generic demos

---
+++ 06_TypeScript_Generics/demo03_GenericInterfaces/main.ts
@@ -0,0 +1,25 @@
+interface IRepository<T>
+{
+  Load():T;
+  Save(item:T):void;
+
+}
+
+class NumberRepo implements IRepository<number>
+{
+  private _number=5;
+  Load():number{
+    return this._number;
+  }
+  Save(item:number):void{ this._number = item;}
+
+}
+
+let repo = new NumberRepo();
+console.log(repo.Load());
+
+repo.Save(13);
+console.log(repo.Load());
+
+
+
---
+++ 06_TypeScript_Generics/demo04_GenericClasses/main.ts
@@ -0,0 +1,28 @@
+interface IRepository<T>
+{
+  Load():T;
+  Save(item:T):void;
+
+}
+
+class GenericRepo<T> implements IRepository<T>
+{
+  private _store:T;
+  constructor(initialValue:T)
+  {
+   this._store=initialValue;
+  }
+  Load():T{
+    return this._store;
+  }
+  Save(item:T):void{ this._store = item;}
+}
+
+let repo = new GenericRepo<number>(5);
+console.log(repo.Load());
+
+repo.Save(13);
+console.log(repo.Load());
+
+
+
