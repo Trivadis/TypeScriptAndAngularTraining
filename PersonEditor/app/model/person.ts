33f52fbd631b1291a811b2f48205911823ac25f0
Thomas Gassmann <thomas.gassmann@windowslive.com>
2016-11-10 14:19:36 +0100

    minor changes

--- PersonEditor/app/model/person.ts
+++ PersonEditor/app/model/person.ts
@@ -1,9 +1,16 @@
-export class Person
-{
-    constructor(
-       public id:number,
-       public firstname:string,
-       public lastname?:string,
-       public githubaccount?:string){
-    }
+// export class Person
+// {
+//     constructor(
+//        public id:number,
+//        public firstname:string,
+//        public lastname?:string,
+//        public githubaccount?:string){
+//     }
+// }
+
+export interface Person {
+    id: number;
+    firstname: string;
+    lastname?: string;
+    githubaccount?: string;
 }
