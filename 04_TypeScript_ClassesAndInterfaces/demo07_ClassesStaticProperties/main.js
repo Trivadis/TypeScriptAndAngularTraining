5780351b3da36e7863806813cf1e52ad5b6996ac
Thomas Bandixen <thomas@bandixen.ch>
2016-11-14 13:20:23 +0100

    formatting

--- 04_TypeScript_ClassesAndInterfaces/demo07_ClassesStaticProperties/main.js
+++ 04_TypeScript_ClassesAndInterfaces/demo07_ClassesStaticProperties/main.js
@@ -2,9 +2,9 @@ var Person = (function () {
     function Person() {
         Person.personCounter++;
     }
+    Person.personCounter = 0;
     return Person;
 }());
-Person.personCounter = 0;
 var p1 = new Person();
 var p2 = new Person();
 var p3 = new Person();
--- 04_TypeScript_ClassesAndInterfaces/demo07_ClassesStaticProperties/main.ts
+++ 04_TypeScript_ClassesAndInterfaces/demo07_ClassesStaticProperties/main.ts
@@ -1,14 +1,13 @@
-class Person
-{
-    static personCounter:number=0;
+class Person {
+    static personCounter: number = 0;
 
-    constructor()
-    {
-       Person.personCounter++;
+    constructor() {
+        Person.personCounter++;
     }
 }
 
 let p1 = new Person();
 let p2 = new Person();
 let p3 = new Person();
+
 console.log(Person.personCounter); // 3
