f476479f554dd7937b4852033f3d17a96e0c4d33
Thomas Bandixen <thomas@bandixen.ch>
2016-11-14 17:54:15 +0100

    enhanced

--- 02_TypeScript_BasicTypes/demo04_tuples/main.js
+++ 02_TypeScript_BasicTypes/demo04_tuples/main.js
@@ -1,6 +1,7 @@
 function writeOutput() {
-    var nameIsDev = ["Thomas", true];
+    var nameIsDev = ["Thomas", true, 42];
     document.write(nameIsDev[0] + "<br>");
     document.write(nameIsDev[1] + "<br>");
+    document.write(nameIsDev[2] + "<br>");
 }
 writeOutput();
--- 02_TypeScript_BasicTypes/demo04_tuples/main.ts
+++ 02_TypeScript_BasicTypes/demo04_tuples/main.ts
@@ -1,8 +1,9 @@
 function writeOutput() {
-    let nameIsDev: [string, boolean] = ["Thomas", true];
+    let nameIsDev: [string, boolean, number] = ["Thomas", true, 42];
 
     document.write(nameIsDev[0] + "<br>");
     document.write(nameIsDev[1] + "<br>");
+    document.write(nameIsDev[2] + "<br>");
 }
 
 writeOutput();
