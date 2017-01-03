569d67d464c7441c066d8376d9e954b6641d72ba
Thomas Gassmann <thomas.gassmann@windowslive.com>
2017-01-03 11:12:43 +0100

    Add Form starter demo

---
+++ 19_Angular_Form/01_demo_Template/starter/app/person-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Person } from './person';
+
+@Component(
+  {
+    selector: 'person-form',
+    styles: ['label {display:block; }'],
+    template: ` 
+      TODO: Bind person object to form and make each input required using template driven approach  
+      <form novalidate>
+          <label>
+            <span>Full name</span>
+            <input type="text" name="name" placeholder="Your full name">
+          </label>
+          <label>
+            <span>Email address</span>
+            <input type="email" name="email" placeholder="Your email">
+          </label>
+          <button type="submit">Save</button>
+      </form>`
+  }
+)
+export class PersonFormComponent {
+  person:Person = {name: 'Thomas', email: 'thomas.gassmann@trivadis.com'}
+}
---
+++ 19_Angular_Form/02_demo_Reactive/starter/app/person.ts
@@ -0,0 +1,5 @@
+export interface Person
+{
+    name:string;
+    email:string;
+}
