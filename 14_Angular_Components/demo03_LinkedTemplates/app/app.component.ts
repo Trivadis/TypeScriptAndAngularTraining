3f3d3f28d8a9bb84c010d51e95f26a1fe33320f3
Thomas Bandixen <thomas@bandixen.ch>
2017-04-11 15:31:39 +0200

    formattings

--- 14_Angular_Components/demo03_LinkedTemplates/app/app.component.ts
+++ 14_Angular_Components/demo03_LinkedTemplates/app/app.component.ts
@@ -1,18 +1,18 @@
+import { Person } from './person';
 import { Component } from '@angular/core';
-import {Person} from './person';
 
 @Component({
   selector: 'my-app',
-  templateUrl:'/app/app.component.html'
+  templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
-   persons:Person[]=PERSONS;
+  persons: Person[] = PERSONS;
 }
 
-var PERSONS:Person[] = [
-  {firstname:'Silvester', lastname:'Stallone'},
-  {firstname:'Thomas', lastname:'Bandixen',githubaccount:'tbandixen'},
-  {firstname:'Thomas', lastname:'Huber',githubaccount:'thomasclaudiushuber'},
-  {firstname:'Bruce', lastname:'Willis'},
-  {firstname:'Lara', lastname:'Croft'}];
-
+var PERSONS: Person[] = [
+  { firstname: 'Silvester', lastname: 'Stallone' },
+  { firstname: 'Thomas', lastname: 'Bandixen', githubaccount: 'tbandixen' },
+  { firstname: 'Thomas', lastname: 'Huber', githubaccount: 'thomasclaudiushuber' },
+  { firstname: 'Bruce', lastname: 'Willis' },
+  { firstname: 'Lara', lastname: 'Croft' }
+];
--- 14_Angular_Components/demo03_LinkedTemplates/app/app.module.ts
+++ 14_Angular_Components/demo03_LinkedTemplates/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
