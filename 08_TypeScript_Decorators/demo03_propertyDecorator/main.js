02978e4a9ced8de7012987e8c51dd4456b8938dc
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2018-05-03 15:34:11 +0200

    Add property decorator

---
+++ 08_TypeScript_Decorators/demo03_propertyDecorator/main.js
@@ -0,0 +1,39 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function logAccess(target, propertyKey) {
+    // grab the value
+    var value = target[propertyKey];
+    // define a setter
+    var setter = function (newValue) {
+        value = newValue;
+        console.log("set " + propertyKey + ": " + value);
+    };
+    // define a getter
+    var getter = function () {
+        console.log("get " + propertyKey + ": " + value);
+        return value;
+    };
+    // replace the property
+    if (delete target[propertyKey]) {
+        Object.defineProperty(target, propertyKey, {
+            get: getter,
+            set: setter
+        });
+    }
+}
+var Friend = /** @class */ (function () {
+    function Friend() {
+    }
+    __decorate([
+        logAccess
+    ], Friend.prototype, "name", void 0);
+    return Friend;
+}());
+var friend = new Friend();
+friend.name = "Thomas";
+friend.name = "Julia";
+var theName = friend.name;
---
+++ 08_TypeScript_Decorators/demo03_propertyDecorator/main.ts
@@ -0,0 +1,36 @@
+function logAccess(target: Object, propertyKey: string) {
+    // grab the value
+    let value = target[propertyKey];
+
+    // define a setter
+    let setter = (newValue) => {
+        value = newValue;
+        console.log(`set ${propertyKey}: ${value}`);
+    };
+
+    // define a getter
+    let getter = () => {
+        console.log(`get ${propertyKey}: ${value}`);
+        return value;
+    };
+
+    // replace the property
+    if (delete target[propertyKey]) {
+        Object.defineProperty(
+            target, propertyKey, {
+                get: getter,
+                set: setter
+            });
+    }
+}
+
+class Friend {
+
+    @logAccess
+    name: string;
+}
+
+let friend = new Friend();
+friend.name = "Thomas";
+friend.name = "Julia";
+let theName = friend.name;
