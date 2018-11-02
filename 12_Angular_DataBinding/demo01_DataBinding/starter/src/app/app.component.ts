14876f68087d80d6b472736fb448b0e8a3a5d14c
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-11-02 21:56:44 +0100

    Update

--- 12_Angular_DataBinding/demo01_DataBinding/starter/src/app/app.component.ts
+++ 12_Angular_DataBinding/demo01_DataBinding/starter/src/app/app.component.ts
@@ -1,10 +1,15 @@
+// TODO: 1. Map the view to a Person instead of string
+//      2. Add TwoWay Data Binding
+//      3. Something is missing => To use ngModel you must import FormsModule in the Root Module (AppModule)
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<h1>Details of {{fullname | uppercase}}</h1>
-            <input type="text" [value]="fullname">`
+            <input type="text" [value]="fullname">
+            `
 })
 export class AppComponent {
-  fullname: string = "Lara Croft";
+  fullname: string = 'Lara Croft';
 }
