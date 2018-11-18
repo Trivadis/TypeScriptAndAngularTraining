57296d071464aaf10350922c44500a452624c4ab
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-11-18 21:01:24 +0100

    Update

--- 16_Angular_Http/demo02_SendingAnHttpRequest_Observables/src/app/person-list/person-list.component.ts
+++ 16_Angular_Http/demo02_SendingAnHttpRequest_Observables/src/app/person-list/person-list.component.ts
@@ -1,36 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Person } from '../person';
 import { PersonDataService } from '../person-data.service';
 
-@Component(
-  {
-    selector: 'app-person-list',
-    template: `
-         <table>
-             <tr>
-               <th>Firstname</th>
-               <th>Lastname</th>
-               <th>Github</th>
-             </tr>
-             <tr *ngFor="let person of persons" (click)="onPersonClick(person)">
-               <td>{{person.firstname}}</td>
-               <td>{{person.lastname}}</td>
-               <td>{{person.githubaccount}}</td>
-             </tr>
-           </table>`
-  }
-)
+@Component({
+  selector: 'app-person-list',
+  template: `
+    <table>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Github</th>
+      </tr>
+      <tr *ngFor="let person of persons" (click)="onPersonClick(person)">
+        <td>{{ person.firstname }}</td>
+        <td>{{ person.lastname }}</td>
+        <td>{{ person.githubaccount }}</td>
+      </tr>
+    </table>
+  `
+})
 export class PersonListComponent implements OnInit {
-
   persons: Person[];
   selectedPerson: Person;
 
-  constructor(private _personDataService: PersonDataService) { }
+  constructor(private _personDataService: PersonDataService) {}
 
   ngOnInit() {
-    this._personDataService.loadPersons()
-      .subscribe(p => this.persons = p,
-        error => alert(error));
+    this._personDataService
+      .loadPersons()
+      .pipe(map(p => p.filter(f => f.firstname === 'Thomas')))
+      .subscribe(p => (this.persons = p), error => alert(error));
   }
 
   onPersonClick(person: Person) {
