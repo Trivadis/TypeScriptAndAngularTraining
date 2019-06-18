59e286406cb24c8d389c729c86c1d8c3124495b3
Thomas Gassmann <thomas.gassmann@windowslive.com>
2019-06-18 15:22:52 +0200

    Update Guard

--- PersonEditor/src/app/guards/person-detail.guard.ts
+++ PersonEditor/src/app/guards/person-detail.guard.ts
@@ -3,21 +3,17 @@ import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 
 @Injectable()
 export class PersonDetailGuard implements CanActivate {
+  constructor(private _router: Router) {}
 
-    constructor(
-        private _router: Router
-    ) {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const id = +route.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert('Invalid Id');
+      // start a new navigation to redirect to list page
+      this._router.navigate(['/persons']);
+      // abort current navigation
+      return false;
     }
-
-    canActivate(route: ActivatedRouteSnapshot): boolean {
-        const id = +route.url[1].path;
-        if (isNaN(id) || id < 1) {
-            alert('Invalid product Id');
-            // start a new navigation to redirect to list page
-            this._router.navigate(['/products']);
-            // abort current navigation
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
