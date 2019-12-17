499cdfd5cdd568be80c4b988c4105dded6472014
Patrik Böschenstein <patrik.boeschenstein@trivadis.com>
2019-12-17 15:39:10 +0100

    add some gitignore, delete js

--- 02_TypeScript_BasicTypes/demo02_stringPlayingWithHtmlElements/main.js
+++
@@ -1,9 +0,0 @@
-function onKeyUp() {
-    // Grab the input element and its value
-    var input = document.getElementById("myInput");
-    var firstname = input.value;
-    // Grab the output element
-    var output = document.getElementById("myOutput");
-    // assign the message to the span-element’s innerText-property
-    output.innerText = "Hi " + firstname + ",\nyour firstname length is " + firstname.trim().length + "!";
-}
