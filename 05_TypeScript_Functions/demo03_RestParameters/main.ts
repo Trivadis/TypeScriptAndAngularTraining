1ee4d87121e1a2b5e5a2fe59d72befdac5251fab
Patrik Böschenstein <patrik.boeschenstein@trivadis.com>
2019-12-17 15:46:08 +0100

    add comment for ... (ES6 Spread Operator)

--- 05_TypeScript_Functions/demo03_RestParameters/main.ts
+++ 05_TypeScript_Functions/demo03_RestParameters/main.ts
@@ -4,4 +4,5 @@ function getFullName(firstName: string, ...moreNames: string[]) {
 
 console.log(getFullName("Thomas"));
 console.log(getFullName("Thomas", "Claudius", "Huber"));
+// ... = ES6 Spread Operator:
 console.log(getFullName("Thomas", ...["Bandixen", "Gassmann"])); // just ["Bandixen", "Gassmann"] wouldn't work 
