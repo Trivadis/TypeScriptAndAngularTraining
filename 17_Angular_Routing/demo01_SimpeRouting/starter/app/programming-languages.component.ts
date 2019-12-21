ca58929c191b2a333db922a3dc620edaef759f00
Patrik Böschenstein <patrik.boeschenstein@trivadis.com>
2019-12-21 14:43:13 +0100

    fix typo

--- 17_Angular_Routing/demo01_SimpeRouting/starter/app/programming-languages.component.ts
+++ 17_Angular_Routing/demo01_SimpeRouting/starter/app/programming-languages.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component(
     {
         selector: 'programming-languages',
-        template: `<h1>Programming lanugages</h1>
+        template: `<h1>Programming languages</h1>
                    <div>
                      There's nothing in here, just another component for routing
                    </div>`
