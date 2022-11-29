b01a863f37ba0e8418609d64ef0151eff84766be
Francesco Leardini <piolag@gmail.com>
2022-11-29 17:20:48 +0100

    Updated angular libs

--- 12_Angular_DataBinding/demo01_DataBinding/done/src/app/app.component.ts
+++ 12_Angular_DataBinding/demo01_DataBinding/done/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<h1>Details of {{person.fullname | uppercase}}</h1>
-            <input type="text"
-           [(ngModel)]="person.fullname">`
+            <input type="text"  [(ngModel)]="person.fullname">`
 })
 export class AppComponent {
   person: Person = { fullname: 'Lara Croft' };
