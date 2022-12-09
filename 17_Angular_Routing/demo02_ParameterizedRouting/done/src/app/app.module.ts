950238f8a0d4566b31bcd917ecd7f3a9dbeecb24
Francesco Leardini <piolag@gmail.com>
2022-12-09 15:20:21 +0100

    Added ng material

--- 17_Angular_Routing/demo02_ParameterizedRouting/done/src/app/app.module.ts
+++ 17_Angular_Routing/demo02_ParameterizedRouting/done/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { PersonDetailComponent } from "./person-detail/person-detail.component";
 import { PersonListComponent } from "./person-list/person-list.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const routes = [
+const routes:Routes = [
   { path: "persons", component: PersonListComponent },
   { path: "person/:id", component: PersonDetailComponent },
   { path: "", redirectTo: "/persons", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), BrowserAnimationsModule],
   declarations: [AppComponent, PersonDetailComponent, PersonListComponent],
   bootstrap: [AppComponent],
 })
--- 17_Angular_Routing/demo02_ParameterizedRouting/starter/src/app/app.module.ts
+++ 17_Angular_Routing/demo02_ParameterizedRouting/starter/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { PersonListComponent } from "./person-list/person-list.component";
 import { PersonDetailComponent } from "./person-detail/person-detail.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [AppComponent, PersonDetailComponent, PersonListComponent],
   bootstrap: [AppComponent],
 })
